c108da6a5271a65ea20e01b8d0e976dbea3fd1da
AmmarCode <ammar.code01@gmail.com>
2021-03-11 12:36:29 -0700

    Feat: Add View Entry

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from markdown2 import Markdown
 
 from . import util
 
@@ -8,3 +9,20 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def convert_to_html(entry):
+    markdown = Markdown()
+    entry_page = util.get_entry(entry)
+    converted_entry = markdown.convert(entry_page) if entry_page else None
+    return converted_entry
+
+def entry(request, entry):
+    converted_entry = convert_to_html(entry)
+    if converted_entry is None:
+        return render(request, "encyclopedia/entry_doesnt_exist.html", {
+            "entry_title": entry.upper()
+        })
+    else:
+        return render(request, "encyclopedia/entry.html", {
+            "entry": converted_entry,
+            "entry_title": entry.upper()
+        })
