2944357e0fac27de39207631500101af7ef21b8f
AmmarCode <ammar.code01@gmail.com>
2021-03-11 13:55:40 -0700

    Feat: Add Creat & Save New Entry

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("wiki/<str:entry>/", views.entry, name="entry")
+    path("wiki/<str:entry>/", views.entry, name="entry"),
+    path("new", views.new, name='new'),
+    path("save", views.save, name='save')
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -26,3 +26,21 @@ def entry(request, entry):
             "entry": converted_entry,
             "entry_title": entry.upper()
         })
+
+def new(request):
+    return render(request, "encyclopedia/new.html")
+
+def save(request):
+    if request.method == 'POST':
+        input_title = request.POST['title']
+        input_text = request.POST['text']
+        entries = util.list_entries()
+        if input_title in entries:
+            return render(request, 'encyclopedia/entry_exists')
+        else:
+            util.save_entry(input_title, input_text)
+            converted_entry = convert_to_html(input_title)
+            return render(request, "encyclopedia/entry.html", {
+                "entry": converted_entry,
+                "entry_title": input_title
+            })
